f5a6c4f5b96c8cf606913cd3dbf27bfc6333ef7a
fantasy2345 <peter.jas0n325@gmail.com>
2022-05-06 16:25:45 +0900

    Oceanan Standard for NFT

---
+++ contracts/NFTCollection.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC1155Oceana.sol";
+
+contract NFTCollection is ERC1155Oceana {
+    using Address for address;
+
+    constructor() {}
+}
