1255008fcac2bb288bac96fde5de98566a5b20d6
ah953 <palindrome0223@gmail.com>
2022-05-11 06:16:41 -0400

    removed comments in current stage

--- contracts/ERC1155Oceana.sol
+++ contracts/ERC1155Oceana.sol
@@ -8,13 +8,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-/**
- * @dev Implementation of the basic standard multi-token.
- * See https://eips.ethereum.org/EIPS/eip-1155
- * Originally based on code by Enjin: https://github.com/enjin/erc-1155
- *
- * _Available since v3.1._
- */
+
 abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
     using Address for address;
 
@@ -26,23 +20,10 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
     mapping(uint256 => mapping(address => mapping(address => bool)))
         private _operatorApprovals;
 
-    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
-    // Used as a custom URI for severl token types
-    // Mapping from favID => tokenID to custom URI
     mapping(uint256 => mapping(uint256 => string)) _uri;
 
-
-    
-    //mapping(uint256 => string) private _uri;
-
-    /**
-     * @dev
-     */
     constructor() {}
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -55,14 +36,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
             super.supportsInterface(interfaceId);
     }
 
-    /**
-     * @dev See {IERC1155Oceanan-uri}.
-     *
-     * @dev Returns the URI for token type `id`.
-     *
-     * If the `\{id\}` substring is present in the URI, it must be replaced by
-     * clients with the actual token type ID.
-     */
     function uri(uint256 favId, uint256 tokenId)
         public
         view
@@ -73,25 +46,11 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         return _uri[favId][tokenId];
     }
 
-    /**
-     * @dev See {IERC1155Oceanan-uri}.
-     *
-     * @dev sets the URI for token type `id` of specific FAV.
-     *
-     */
-
     function _setURI(uint256 favId, uint256 tokenId, string memory newUri) internal virtual{
         _uri[favId][tokenId] = newUri;
         emit URI(favId, tokenId, newUri);
     }
 
-    /**
-     * @dev See {IERC1155Oceana-balanceOf}.
-     *
-     * Requirements:
-     *
-     * - `account` cannot be the zero address.
-     */
     function balanceOf(
         uint256 favId,
         address account,
@@ -104,13 +63,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         return _balances[favId][id][account];
     }
 
-    /**
-     * @dev See {IERC1155Oceana-balanceOfBatch}.
-     *
-     * Requirements:
-     *
-     * - `accounts` and `ids` must have the same length.
-     */
     function balanceOfBatch(
         uint256 favId,
         address[] memory accounts,
@@ -130,9 +82,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         return batchBalances;
     }
 
-    /**
-     * @dev See {IERC1155Oceana-setApprovalForAll}.
-     */
     function setApprovalForAll(
         uint256 favId,
         address operator,
@@ -141,9 +90,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         _setApprovalForAll(favId, _msgSender(), operator, approved);
     }
 
-    /**
-     * @dev See {IERC1155Oceana-isApprovedForAll}.
-     */
     function isApprovedForAll(
         uint256 favId,
         address account,
@@ -152,9 +98,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         return _operatorApprovals[favId][account][operator];
     }
 
-    /**
-     * @dev See {IERC1155Oceana-safeTransferFrom}.
-     */
     function safeTransferFrom(
         address from,
         address to,
@@ -170,9 +113,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         _safeTransferFrom(from, to, favId, id, amount, data);
     }
 
-    /**
-     * @dev See {IERC1155Oceana-safeBatchTransferFrom}.
-     */
     function safeBatchTransferFrom(
         address from,
         address to,
@@ -189,18 +129,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         _safeBatchTransferFrom(from, to, favId, ids, amounts, data);
     }
 
-    /**
-     * @dev Transfers `amount` tokens of token type `id` from `from` to `to`.
-     *
-     * Emits a {TransferSingle} event.
-     *
-     * Requirements:
-     *
-     * - `to` cannot be the zero address.
-     * - `from` must have a balance of tokens of type `id` of at least `amount`.
-     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received} and return the
-     * acceptance magic value.
-     */
     function _safeTransferFrom(
         address from,
         address to,
@@ -234,16 +162,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         _doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
     }
 
-    /**
-     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {_safeTransferFrom}.
-     *
-     * Emits a {TransferBatch} event.
-     *
-     * Requirements:
-     *
-     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
-     * acceptance magic value.
-     */
     function _safeBatchTransferFrom(
         address from,
         address to,
@@ -291,18 +209,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         );
     }
 
-
-    /**
-     * @dev Creates `amount` tokens of token type `id`, and assigns them to `to`.
-     *
-     * Emits a {TransferSingle} event.
-     *
-     * Requirements:
-     *
-     * - `to` cannot be the zero address.
-     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received} and return the
-     * acceptance magic value.
-     */
     function _mint(
         address to,
         uint256 favId,
@@ -349,15 +255,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         );
     }
 
-    /**
-     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {_mint}.
-     *
-     * Requirements:
-     *
-     * - `ids` and `amounts` must have the same length.
-     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
-     * acceptance magic value.
-     */
     function _mintBatch(
         address to,
         uint256 favId,
@@ -409,14 +306,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         );
     }
 
-    /**
-     * @dev Destroys `amount` tokens of token type `id` from `from`
-     *
-     * Requirements:
-     *
-     * - `from` cannot be the zero address.
-     * - `from` must have at least `amount` tokens of token type `id`.
-     */
     function _burn(
         address from,
         uint256 favId,
@@ -458,13 +347,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         );
     }
 
-    /**
-     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {_burn}.
-     *
-     * Requirements:
-     *
-     * - `ids` and `amounts` must have the same length.
-     */
     function _burnBatch(
         address from,
         uint256 favId,
@@ -516,11 +398,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         );
     }
 
-    /**
-     * @dev Approve `operator` to operate on all of `owner` tokens
-     *
-     * Emits a {ApprovalForAll} event.
-     */
     function _setApprovalForAll(
         uint256 favId,
         address owner,
@@ -532,26 +409,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         emit ApprovalForAll(favId, owner, operator, approved);
     }
 
-    /**
-     * @dev Hook that is called before any token transfer. This includes minting
-     * and burning, as well as batched variants.
-     *
-     * The same hook is called on both single and batched variants. For single
-     * transfers, the length of the `id` and `amount` arrays will be 1.
-     *
-     * Calling conditions (for each `id` and `amount` pair):
-     *
-     * - When `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * of token type `id` will be  transferred to `to`.
-     * - When `from` is zero, `amount` tokens of token type `id` will be minted
-     * for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens of token type `id`
-     * will be burned.
-     * - `from` and `to` are never both zero.
-     * - `ids` and `amounts` have the same, non-zero length.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
     function _beforeTokenTransfer(
         address operator,
         address from,
@@ -562,26 +419,6 @@ abstract contract ERC1155Oceana is Context, ERC165, IERC1155Oceana {
         bytes memory data
     ) internal virtual {}
 
-    /**
-     * @dev Hook that is called after any token transfer. This includes minting
-     * and burning, as well as batched variants.
-     *
-     * The same hook is called on both single and batched variants. For single
-     * transfers, the length of the `id` and `amount` arrays will be 1.
-     *
-     * Calling conditions (for each `id` and `amount` pair):
-     *
-     * - When `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * of token type `id` will be  transferred to `to`.
-     * - When `from` is zero, `amount` tokens of token type `id` will be minted
-     * for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens of token type `id`
-     * will be burned.
-     * - `from` and `to` are never both zero.
-     * - `ids` and `amounts` have the same, non-zero length.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
     function _afterTokenTransfer(
         address operator,
         address from,
