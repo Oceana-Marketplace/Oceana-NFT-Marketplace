92a7e63663663065d5bf0532fec2ae54c5b03078
chainlito <okura.ikeda0325@gmail.com>
2022-05-11 16:30:02 +0900

    Add: setTokenUri function

---
+++ .env.js
@@ -0,0 +1,16 @@
+const credentials = {
+  privateKey:
+    "e91e66190024c58fcb9392204b563973d5050115ece8bf6f55d4a0e1db284ac1",
+  Infura: "fe3d3df39a794cf090c230ba22041205",
+  alchemy: "",
+  etherscan: "GDR94RMJUU35AFG1BHPZ1DMAA7RIA5NF4U",
+  coinmarketcap: "",
+  defenderApi: "",
+  defenderSecret: "",
+  MNEMONIC_PHRASE:
+    "umbrella runway truth girl gap fashion tourist acquire harsh great camera mobile",
+  ownerAddr: "0xBEE7764727e7FeACC9C640Ae6AC0809404C491Fa",
+  testAddr: "0xfd40DBC86ba7Da3f60538661B50c58Cb087E978D",
+};
+
+module.exports = credentials;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-abi-exporter");
+
+const credentials = require("./.env.js");
+const INFURA_PROJECT_ID = credentials.Infura;
+const METAMASK_PRIVATE_KEY = credentials.privateKey;
+const ETHERSCAN_KEY = credentials.etherscan;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +25,58 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  abiExporter: {
+    path: "./abi",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    only: [],
+    spacing: 0,
+    pretty: false,
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_KEY}`,
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mainnet: {
+      chainId: 1,
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      //			url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API}`,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`],
+      //		gas: 7000000,
+      skipDryRun: true,
+    },
+    rinkeby: {
+      chainId: 4,
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`],
+      gas: 7000000,
+      skipDryRun: true,
+    },
+    kovan: {
+      chainId: 42,
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`],
+      gas: 7000000,
+      skipDryRun: true,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
