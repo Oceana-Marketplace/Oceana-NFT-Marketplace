80c8e23a418d146d71b018a7c93b81e5f6792b69
ah953 <palindrome0223@gmail.com>
2022-05-11 06:38:17 -0400

    Add: event to setFavDataUrl

--- contracts/IERC1155Oceana.sol
+++ contracts/IERC1155Oceana.sol
@@ -1,7 +1,20 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (token/ERC1155/IERC1155.sol)
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * @dev Required interface of an ERC1155 compliant contract, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-1155[EIP].
+ *
+ * _Available since v3.1._
+ */
 interface IERC1155Oceana is IERC165 {
+    /**
+     * @dev Emitted when `value` tokens of token type `id` of fav `favId` are transferred from `from` to `to` by `operator`.
+     */
     event TransferSingle(
         address indexed operator,
         address indexed from,
@@ -11,6 +24,10 @@ interface IERC1155Oceana is IERC165 {
         uint256 value
     );
 
+    /**
+     * @dev Equivalent to multiple {TransferSingle} events, where `operator`, `from` and `to`, `favId` are the same for all
+     * transfers.
+     */
     event TransferBatch(
         address indexed operator,
         address indexed from,
@@ -19,6 +36,11 @@ interface IERC1155Oceana is IERC165 {
         uint256[] ids,
         uint256[] values
     );
+
+    /**
+     * @dev Emitted when `account` grants or revokes permission to `operator` to transfer their tokens, according to
+     * `approved`.
+     */
     event ApprovalForAll(
         uint256 favId,
         address indexed account,
@@ -26,32 +48,80 @@ interface IERC1155Oceana is IERC165 {
         bool approved
     );
 
+    /**
+     * @dev Emitted when the URI for token type `id` changes to `value`, if it is a non-programmatic URI.
+     *
+     * If an {URI} event was emitted for `id`, the standard
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata-extensions[guarantees] that `value` will equal the value
+     * returned by {IERC1155MetadataURI-uri}.
+     */
     event URI(uint256 indexed favId, uint256 indexed id, string value);
+
+    /**
+     * @dev Returns the amount of tokens of token type `id` at `favID` marketplace owned by `account` .
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
     function balanceOf(
         uint256 favId,
         address account,
         uint256 id
     ) external view returns (uint256);
 
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {balanceOf}.
+     *
+     * Requirements:
+     *
+     * - `accounts` and `ids` must have the same length.
+     */
     function balanceOfBatch(
         uint256 favId,
         address[] calldata accounts,
         uint256[] calldata ids
     ) external view returns (uint256[] memory);
 
+    /**
+     * @dev Grants or revokes permission to `operator` to transfer the caller's tokens, according to `approved`,
+     *
+     * Emits an {ApprovalForAll} event.
+     *
+     * Requirements:
+     *
+     * - `operator` cannot be the caller.
+     */
     function setApprovalForAll(
         uint256 favId,
         address operator,
         bool approved
     ) external;
 
-
+    /**
+     * @dev Returns true if `operator` is approved to transfer ``account``'s tokens.
+     *
+     * See {setApprovalForAll}.
+     */
     function isApprovedForAll(
         uint256 favId,
         address account,
         address operator
     ) external view returns (bool);
 
+    /**
+     * @dev Transfers `amount` tokens of token type `id` from `from` to `to`.
+     *
+     * Emits a {TransferSingle} event.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - If the caller is not `from`, it must be have been approved to spend ``from``'s tokens via {setApprovalForAll}.
+     * - `from` must have a balance of tokens of type `id` of at least `amount`.
+     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received} and return the
+     * acceptance magic value.
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -61,6 +131,17 @@ interface IERC1155Oceana is IERC165 {
         bytes calldata data
     ) external;
 
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {safeTransferFrom}.
+     *
+     * Emits a {TransferBatch} event.
+     *
+     * Requirements:
+     *
+     * - `ids` and `amounts` must have the same length.
+     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
+     * acceptance magic value.
+     */
     function safeBatchTransferFrom(
         address from,
         address to,
@@ -70,5 +151,11 @@ interface IERC1155Oceana is IERC165 {
         bytes calldata data
     ) external;
 
+    /**
+     * @dev Returns the URI for token type `id`.
+     *
+     * If the `\{id\}` substring is present in the URI, it must be replaced by
+     * clients with the actual token type ID.
+     */
     function uri(uint256 favId, uint256 tokenId) external view returns (string memory);
 }
