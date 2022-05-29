f21c144b2fb3c7e006bfd11cb421100353e1a228
ah953 <palindrome0223@gmail.com>
2022-05-29 17:16:51 -0400

    remove: .env.js

--- .env.js
+++
@@ -1,8 +0,0 @@
-const credentials = {
-  Infura: "c919c911f9a346f8b93a9feb8ad1c3c3",
-  privateKey:
-    "af657925c49008933f1b0ec82f47c8fb17a23779c11dd88c4ee2d1d4e012c544",
-  etherscan: "I4HMTFZMKT4B427EZBK6KMI339VFEGN6YE",
-};
-
-module.exports = credentials;
